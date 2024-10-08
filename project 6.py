c2b8d2b1459413ef295f24d021b17a56752ac4e2
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-10-08 15:27:25 +0530

    Add files via upload

---
+++ project 6.py
@@ -0,0 +1,21 @@
+'''
+author:Nicole Nelson
+date:08-10-2024
+python program to calculate arithmetic calculations
+version 1.0
+'''
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+sum=num1+num2
+print("the sum of the num1 and num2 is:",sum)
+difference=num2-num1
+print("The difference when num2 is subtracted from num1 is:",difference)
+multiplication=num1*num2
+print("The product of num1 and num2 is:",multiplication)
+division=num1/num2
+print("The quotient when num1 is divided by num2 is:",division)
+modulus=num1%num2
+print("The remainder when num1 is divided by num2 is:",modulus)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is:",result)
